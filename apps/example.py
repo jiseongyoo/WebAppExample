da3d0f1ee410787aeec9ac0bb9cbdb00149039db
jiseong-yoo <jsing89@gmail.com>
2024-08-01 21:56:12 +0900

    /test 혹은 /example로 post를 받으면 /third로 데이터를 보내는 내용

---
+++ apps/example.py
@@ -0,0 +1,26 @@
+from flask import jsonify
+import requests
+import json
+
+class Example:
+    def run(self, request):
+        params = request.get_json()
+        print("받은 Json 데이터 ", params)
+
+        self.post(request)
+
+        response = {
+            "result": "example"
+        }
+
+        return jsonify(response)
+
+    def post(self, request):
+        url = "http://127.0.0.1:8000/third"
+        headers = {"Content-Type": "application/json"}
+        temp = request.get_json()
+        data = json.dumps(temp)
+
+        response = requests.post(url, headers=headers, data=data)
+        print("response: ", response)
+        print("response.text: ", response.text)
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from flask import Flask, request, json, jsonify
+from flask import Flask, request
+from apps.test import Test
+from apps.example import Example
 
 app = Flask(__name__)
 
+
 @app.route("/test", methods=['POST'])
 def test():
-    params = request.get_json()
-    print("받은 Json 데이터 ", params)
+    return Test().run(request)
 
-    response = {
-        "result": "ok"
-    }
 
-    return jsonify(response)
+@app.route("/example", methods=['POST'])
+def example():
+    return Example().run(request)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
