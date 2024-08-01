7943faee4cbf23e89282505ed23f6fcfdc2f3759
jiseong-yoo <jsing89@gmail.com>
2024-08-01 21:21:24 +0900

    example implementation

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Web App Example using Flask
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+from flask import Flask, request, json, jsonify
+
+app = Flask(__name__)
+
+@app.route("/test", methods=['POST'])
+def test():
+    params = request.get_json()
+    print("받은 Json 데이터 ", params)
+
+    response = {
+        "result": "ok"
+    }
+
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
